09550cf54a793b385606305e526c595e3973418d
liuyong <liuy@yimym.com>
2018-08-21 22:08:13 +0800

    提交

--- index.js
+++ index.js
@@ -26,42 +26,68 @@ function Worker(name, pay, restaurant) {
     while(ids.indexOf(id) > -1) {
         id = Math.floor(Math.random() * 100 + 1);
     }
-    console.log("id", id);
+    // console.log("id", id);
     this.id = id;
 }
 Worker.prototype.finishWorker = function() {}
  
-function Waiter(name, pay, restaurant) {
-    Worker.call(this, name, pay, restaurant);
-}
-Waiter.prototype.finishWorker = function(arg) {
-    if(Array.isArray(arguments[0])) {
-        console.log("记录菜品");
-    } else {
-        console.log("完成上菜")
+var waiterSingleton = (function() {
+    var instance = null;
+    function Waiter(name, pay, restaurant) {
+        Worker.call(this, name, pay, restaurant);
     }
-}
+    Waiter.prototype.finishWorker = function(arg) {
+        if(Array.isArray(arguments[0])) {
+            console.log("记录菜品", arg[0].name);
+            return arg;
+        } else {
+            console.log("完成上菜")
+        }
+    }
 
-function Cook(name, pay, restaurant) {
-    Worker.call(this,  name, pay, restaurant);
-}
-Cook.prototype.finishWorker = function(food) {
+    return function(name, pay, restaurant) {
+        if(!instance) {
+            instance = new Waiter(name, pay, restaurant)
+        }
+        return instance
+    }
+})()
 
-}
+var cookSingleton = (function() {
+    var instance = null;
+    function Cook(name, pay, restaurant) {
+        Worker.call(this,  name, pay, restaurant);
+    }
+    Cook.prototype.finishWorker = function(food) {
+        console.log("烧完菜" + food[0].name)
+        return food;
+    }
+
+    return function(name, pay, restaurant) {
+        if(!instance) {
+            instance = new Cook(name, pay, restaurant)
+        }
+        return instance
+    }
+})()
 
 function Customer() {
     this.consumption = 0;
     this.food = [];
 }
-Customer.prototype.order = function(food, restaurant) {//food is array
-    this.food = food;
-    for(var i = 0; i < food.length; i++) {
-        this.consumption += food[i].price;
+Customer.prototype.order = function(restaurant, foodList) {
+    var index =  Math.floor((Math.random() * 10));
+    var cost = 0;
+    this.food = [foodList[index]]
+    for(let i = 0; i < this.food.length; i++) {
+        this.consumption += this.food[i].price;
+        cost += this.food[i].cost;
     }
-    restaurant.cash += this.consumption;
+    restaurant.cash += this.consumption - cost;
+    console.log("customer order")
 }
 Customer.prototype.eat = function(food) {
-
+    console.log("customer eat it up")
 }
  
 function foods(name, cost, price) {
@@ -76,20 +102,27 @@ var ifeRestaurant = new Restaurant({
     seats: 20,
     staff: []
 });
-// var newWorker = new Worker("bob", 10000, ifeRestaurant);
-// ifeRestaurant.hire(newWorker);
-// console.log("hire", ifeRestaurant.staff);
-// ifeRestaurant.fire(newWorker);
-// console.log("fire", ifeRestaurant.staff);
-const workNames = ["jim", "bob", "tom", "alice", "lily"];
-const workPays = [10000, 8000, 8000, 7000, 6000];
-for(var i = 0; i < 5; i++) {
-    if(i < 3) {
-        ifeRestaurant.hire(new Cook(workNames[i], workPays[i], ifeRestaurant));
-    } else {
-        ifeRestaurant.hire(new Waiter(workNames[i], workPays[i], ifeRestaurant))
-    }
+var foodList = [];
+var foodsName = ["qingcai", "huanggua", "nangua", "niurou", "jirou", "paigu", "qiezi", "jiucai", "zhuti", "zhuyaozi"];
+var foodsCost = [20, 20, 20, 30, 30, 30, 20, 20, 30, 30];
+var foodsPrice = [30, 30, 30, 40, 40, 40, 30, 30, 40, 40];
+for(let i = 0; i < 10; i++) {
+    foodList.push(new foods(foodsName[i], foodsCost[i], foodsPrice[i]));
 }
-console.log("hire", ifeRestaurant.staff)
-ifeRestaurant.fire(ifeRestaurant.staff[2])
-console.log("fire", ifeRestaurant.staff)
+var customerList = [];
+for(let i = 0; i < 8; i++) {
+    customerList.push(new Customer());
+}
+
+var waiter = waiterSingleton("alice", 10000, ifeRestaurant);
+var cook = cookSingleton("bob", 10000, ifeRestaurant);
+ifeRestaurant.hire(waiter);
+ifeRestaurant.hire(cook);
+
+for(let i = 0; i < customerList.length; i++) {
+    customerList[i].order(ifeRestaurant, foodList);
+    var food = waiter.finishWorker(customerList[i].food);
+    var cookedFood = cook.finishWorker(food);
+    waiter.finishWorker();
+    customerList[i].eat();
+}
